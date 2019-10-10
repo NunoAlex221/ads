1bbc24d89e7abf56d5edf83f386539b7dc4aeab0
Nuno <nft.mega@gmail.com>
2019-10-10 16:44:08 +0100

    Changed

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -2,6 +2,6 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.WriteLine("Teste Initial Commit")
     End Sub
 End Module
